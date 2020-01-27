2d8e2152ee1e4d571cae083d831f09df32ae499e
ruslan <ruslans.fjodorovs@gmail.com>
2020-01-27 20:09:26 +0200

    hello world

---
+++ WebStore2/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore2.Models;
+
+namespace WebStore2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Random()
+        {
+            var product = new Product()
+            {
+                Name = "proudct 1"
+            };
+
+            return View(product);
+        }
+    }
+}
